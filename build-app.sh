de14aa510c70f0bf2d164d89bcddb28884284c8e
Randy Bijsterbosch <randybijsterbosch@live.nl>
2025-08-11 20:25:35 +0200

    Added helper script to build and deploy app.

---
+++ build-app.sh
@@ -0,0 +1,1 @@
+sudo docker run -it --rm -v $(pwd)/workdir:/workdir rpibuilder --workdir=/workdir ./build-app.sh
---
+++ deploy-app.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Variables
+APP_NAME="clockapp"               # systemd service name
+LOCAL_APP_PATH="workdir/build/tmp/work/cortexa76-poky-linux/clockapp/master/package/usr/bin/clockapp"     # local path to the app binary
+REMOTE_USER="root"                     # remote device username
+REMOTE_HOST="192.168.1.225"    # remote device IP or hostname
+REMOTE_TEMP_PATH="/tmp/$APP_NAME"      # temporary location on remote device
+REMOTE_TARGET_PATH="/usr/bin/$APP_NAME"
+
+# Copy app to remote device
+echo "Copying $APP_NAME to $REMOTE_HOST..."
+scp "$LOCAL_APP_PATH" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_TEMP_PATH"
+
+# SSH into remote device and deploy
+ssh "$REMOTE_USER@$REMOTE_HOST" << EOF
+    echo "Stopping $APP_NAME..."
+    systemctl stop "$APP_NAME"
+
+    echo "Moving app to $REMOTE_TARGET_PATH..."
+    mv "$REMOTE_TEMP_PATH" "$REMOTE_TARGET_PATH"
+    chmod +x "$REMOTE_TARGET_PATH"
+
+    echo "$APP_NAME deployed successfully."
+    systemctl start "$APP_NAME"
+EOF
+
